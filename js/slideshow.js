df7d3b14b3cd343dba478d8900234a785e2a6522
wildmeese <45549321+wildmeese@users.noreply.github.com>
2019-01-11 15:18:15 -0800

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,8 +3,8 @@
             var i = 2; // Counter In
             var o = 1; // Counter Out
             function cycleImages() {
-                $("#slide" + o).fadeOut("slow");
-                $("#slide" + i).fadeIn("slow");
+                $("#slide" + o).fadeOut(2000);
+                $("#slide" + i).fadeIn("fast");
                 o = i;
                 if (i < ni) {
                     i = i + 1;
