e0d97863746705eb77be3ad9914b8e93bf2b584b
Markel F <markelf@protonmail.com>
2019-03-24 23:07:53 -0700

    Follow us section

--- js/instagram_deleter.js
+++ js/instagram_deleter.js
@@ -1,6 +1,7 @@
 // When the window resizes the original Instagram gallery is buggy and must be deleted
 function delete_gallery() {
     document.getElementById("gallery").style.display = "none";
+    document.getElementById("extra_photo").style.display = "flex";
 }
 
 // Initializer
