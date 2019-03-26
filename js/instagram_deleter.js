5f0ea5be51ba2c84b81a1870a277183f8c779749
Markel F <markelf@protonmail.com>
2019-03-26 13:47:10 -0700

    Delete window.onresize
    For improving compatibility with mobile, as hiding the URL bar caused the onresize() event to trigger I have removed this event.
    
    This causes that if you enter to the web in a small window and later maximize the iframe doesn't work properly

--- js/instagram_deleter.js
+++
@@ -1,8 +0,0 @@
-// When the window resizes the original Instagram gallery is buggy and must be deleted
-function delete_gallery() {
-    document.getElementById("gallery").style.display = "none";
-    document.getElementById("alternative_photo").style.display = "initial";
-}
-
-// Initializer
-window.onresize = delete_gallery;
